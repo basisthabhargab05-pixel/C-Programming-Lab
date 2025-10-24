6cced33ed9b724063bd7837b8d3ab370f584e03b
basisthabhargab05-pixel <basisthabhargab05@gmail.com>
2025-10-25 02:16:10 +0530

    Add files via upload
    vivration

---
+++ Lab-3/vivration.c
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+int main()
+{
+	float vib_amp,freq;//calculate vibration.
+	printf("enter the vibration amplitude in mm:");
+	scanf("%f",&vib_amp);
+	printf("enter the frequency in Hz:");
+	scanf("%f",&freq);
+	if(vib_amp>0.5 && (freq>=20 && freq<=50))
+	{
+		printf("HIGH RISK!!!");
+	}
+	else if(vib_amp>0.5 || freq>60)
+	{
+		printf("MEDIUM RISK!!");
+	}
+	else
+	{
+		printf("LOW RISK!");
+	}
+	return 0;
+}
+
