7f9b6ddef7bec13db9ca491343015ba0a363b2c9
basisthabhargab05-pixel <basisthabhargab05@gmail.com>
2025-10-25 00:43:33 +0530

    Add files via upload
    introduction

---
+++ Lab 1/myself.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main ()
+{
+   printf("Myself Bhargab Basistha\n");
+   printf("My roll no is MEB25022\n");
+   printf("My department is Mechanical Engineering\n");
+   printf("My University is Tezpur University");
+   return 0;
+}
