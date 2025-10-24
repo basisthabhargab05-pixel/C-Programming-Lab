90411a54f880fb2f7f6ff36956d57db98947accb
basisthabhargab05-pixel <basisthabhargab05@gmail.com>
2025-10-25 00:34:38 +0530

    Add files via upload
    n*(n+1)/2

---
+++ Assignment 1/sum of natural number.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int n,sum ;
+    printf("enter the number :");
+    scanf("%d",&n);
+    sum=n*(n+1)/2;
+    printf("the sum of the first n natural number:%d",sum);
+    return 0;
+}
