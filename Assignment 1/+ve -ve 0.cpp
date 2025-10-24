23f9db1b64be7439baabb0995ac204133b24c246
basisthabhargab05-pixel <basisthabhargab05@gmail.com>
2025-10-25 00:25:16 +0530

    Add files via upload
    +ve -ve 0

---
+++ Assignment 1/+ve -ve 0.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+
+   int a;
+   printf("enter the number :");
+   scanf("%d",&a);
+   if (a>0)
+   {
+   
+   printf("the number is positive");
+}
+   else if (a<0)
+   {
+   
+   printf("the number is negative");
+}
+   else
+   {
+   printf("the number is zero");
+}
+   return 0;
+}
+
